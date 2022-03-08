61b9ef589a6eb1906d79714dc5ca84fdaef9dcce
SadScheme <abhinavtan90@gmail.com>
2022-03-08 13:15:39 +0000

    a

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 // import './index.css';
 import App from './App';
 // import reportWebVitals from './reportWebVitals';
-import './normalize.css'
+//import './normalize.css'
 //import { BrowserRouter } from 'react-router-dom';
 import { HashRouter as Router } from 'react-router-dom'
 
