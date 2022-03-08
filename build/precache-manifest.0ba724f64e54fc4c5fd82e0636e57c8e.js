7573a5fe390ea0890f34e77d6b6ea3752186a570
SadScheme <abhinavtan90@gmail.com>
2022-03-08 12:35:18 +0000

    Adding production files

---
+++ build/precache-manifest.0ba724f64e54fc4c5fd82e0636e57c8e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "021f4421eee02539b053",
+    "url": "/static/css/main.da3c8f2d.chunk.css"
+  },
+  {
+    "revision": "021f4421eee02539b053",
+    "url": "/static/js/main.021f4421.chunk.js"
+  },
+  {
+    "revision": "007c82ed7d04fb2ee74e",
+    "url": "/static/js/1.007c82ed.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d9e3ce36c1cbbc1af680ebadcd4349ab",
+    "url": "/static/media/profile-img.d9e3ce36.png"
+  },
+  {
+    "revision": "20af9f2c2cc243bdbf8230bd772cd8b3",
+    "url": "/static/media/spaceman.20af9f2c.png"
+  },
+  {
+    "revision": "c5f5e854ee487ef19aa2a4fa6aabbfa9",
+    "url": "/static/media/patrick-tomasso-Oaqk7qqNh_c-unsplash.c5f5e854.jpg"
+  },
+  {
+    "revision": "3a5ccf2f0bb0e6de6d5640ee69bb79ae",
+    "url": "/static/media/u-said-it-v13-1167.3a5ccf2f.mp3"
+  },
+  {
+    "revision": "50d10fdb89e2eee2be4e8e4b619968d1",
+    "url": "/index.html"
+  }
+];
