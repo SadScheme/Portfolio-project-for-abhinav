7d1930c41d539e4e5f2e3c0b928e4d56b9e11c36
SadScheme <abhinavtan90@gmail.com>
2022-03-08 13:08:45 +0000

    build changes

---
+++ build/precache-manifest.afdbc239df5e63e3650b44a8e0c0efd7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "d9b992d180ce9071c930",
+    "url": "/Portfolio-project-for-abhinav/static/css/main.da3c8f2d.chunk.css"
+  },
+  {
+    "revision": "d9b992d180ce9071c930",
+    "url": "/Portfolio-project-for-abhinav/static/js/main.d9b992d1.chunk.js"
+  },
+  {
+    "revision": "8d0f5d1133dd86002f6d",
+    "url": "/Portfolio-project-for-abhinav/static/js/1.8d0f5d11.chunk.js"
+  },
+  {
+    "revision": "e6e74c88e14a09250b73",
+    "url": "/Portfolio-project-for-abhinav/static/js/runtime~main.e6e74c88.js"
+  },
+  {
+    "revision": "d9e3ce36c1cbbc1af680ebadcd4349ab",
+    "url": "/Portfolio-project-for-abhinav/static/media/profile-img.d9e3ce36.png"
+  },
+  {
+    "revision": "20af9f2c2cc243bdbf8230bd772cd8b3",
+    "url": "/Portfolio-project-for-abhinav/static/media/spaceman.20af9f2c.png"
+  },
+  {
+    "revision": "c5f5e854ee487ef19aa2a4fa6aabbfa9",
+    "url": "/Portfolio-project-for-abhinav/static/media/patrick-tomasso-Oaqk7qqNh_c-unsplash.c5f5e854.jpg"
+  },
+  {
+    "revision": "3a5ccf2f0bb0e6de6d5640ee69bb79ae",
+    "url": "/Portfolio-project-for-abhinav/static/media/u-said-it-v13-1167.3a5ccf2f.mp3"
+  },
+  {
+    "revision": "2aa7539a89f29ca42544a1d08223603c",
+    "url": "/Portfolio-project-for-abhinav/index.html"
+  }
+];
