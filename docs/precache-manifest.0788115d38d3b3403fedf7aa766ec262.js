c4cbe9b31591d2f24ee83dc54ab6ac9b4839cec8
SadScheme <abhinavtan90@gmail.com>
2022-05-25 00:22:06 +0100

    moving build to docs

---
+++ docs/precache-manifest.0788115d38d3b3403fedf7aa766ec262.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "1016a96ca2594e56a2d3",
+    "url": "/static/css/main.da3c8f2d.chunk.css"
+  },
+  {
+    "revision": "1016a96ca2594e56a2d3",
+    "url": "/static/js/main.1016a96c.chunk.js"
+  },
+  {
+    "revision": "007c82ed7d04fb2ee74e",
+    "url": "/static/js/1.007c82ed.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d9e3ce36c1cbbc1af680ebadcd4349ab",
+    "url": "/static/media/profile-img.d9e3ce36.png"
+  },
+  {
+    "revision": "20af9f2c2cc243bdbf8230bd772cd8b3",
+    "url": "/static/media/spaceman.20af9f2c.png"
+  },
+  {
+    "revision": "c5f5e854ee487ef19aa2a4fa6aabbfa9",
+    "url": "/static/media/patrick-tomasso-Oaqk7qqNh_c-unsplash.c5f5e854.jpg"
+  },
+  {
+    "revision": "3a5ccf2f0bb0e6de6d5640ee69bb79ae",
+    "url": "/static/media/u-said-it-v13-1167.3a5ccf2f.mp3"
+  },
+  {
+    "revision": "83e3b8729cc0cd5534a1f69c92ad2530",
+    "url": "/index.html"
+  }
+];
